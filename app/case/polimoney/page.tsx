b92335deff5b91ad6ec7089ae3e377c641c8af87
Nozomi-M21 <1999.n.mitarashi@gmail.com>
2025-05-25 11:20:21 +0900

    活用事例の追加（いでいさん）

--- app/case/polimoney/page.tsx
+++ app/case/polimoney/page.tsx
@@ -9,11 +9,29 @@ export default async function Page() {
         <h3 className="text-2xl mt-8">2024年：安野貴博さん（AIエンジニア）の活動</h3>
         <p className="mt-4">
           AIエンジニアの安野貴博さんがPolimoneyを活用し、「2024年
-          デジタル民主主義を考える会」での資金の収支を公開しました。
+          デジタル民主主義を考える会」での資金収支を公開しました。
         </p>
         <div className="mt-4">
           <a
             href="https://polimoney.dd2030.org/demo-takahiro-anno-2024"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`${buttonVariants({
+              variant: 'outline',
+            })} h-9 border-black inline-flex items-center gap-2`}
+          >
+            事例を見る
+            <NavigateNextIcon />
+          </a>
+        </div>
+        <h3 className="text-2xl mt-8">2024年：出井良輔さん（自由民主党）の活動</h3>
+        <p className="mt-4">
+          自由民主党の出井良輔さんがPolimoneyを活用し、「2024年
+          自由民主党東京都中野区第二十支部」での資金収支を公開しました。
+        </p>
+        <div className="mt-4">
+          <a
+            href="https://polimoney.dd2030.org/demo-ryosuke-idei-2024"
             target="_blank"
             rel="noopener noreferrer"
             className={`${buttonVariants({
